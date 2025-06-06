b8ef5e22037300fb9ace1719a841a5c7df65681d
Petrushevskiy Maxim <maxinverst@gmail.com>
2025-06-06 20:11:30 +0500

    add NotFoundPage,pageLoader,Loader

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,20 +4,21 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { AppRouter } from './providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import { Suspense } from 'react';
+// import { Suspense } from 'react';
+// import { PageLoader } from 'widgets/PageLoader/ui/PageLoader';
 
 const App = () => {
     const { theme } = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <Suspense fallback=''>
-                <Navbar />
-                <div className='content-page'>
-                    <Sidebar />
-                    <AppRouter />
-                </div>
-            </Suspense>
+            {/* <Suspense fallback={<PageLoader />}> */}
+            <Navbar />
+            <div className='content-page'>
+                <Sidebar />
+                <AppRouter />
+            </div>
+            {/* </Suspense> */}
         </div>
     );
 };
