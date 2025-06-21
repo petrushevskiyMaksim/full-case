ec08f89ad296d2e95cbd0133e29f7f5dac6a150a
Petrushevskiy Maxim <maxinverst@gmail.com>
2025-06-21 14:56:56 +0500

    fix sidebar test mock

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -2,6 +2,11 @@ import { fireEvent, screen } from '@testing-library/react';
 import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
 import { Sidebar } from 'widgets/Sidebar/ui/Sidebar/Sidebar';
 
+jest.mock('./Sidebar.module.scss', () => ({
+    __esModule: true,
+    collapsed: 'collapsed',
+}));
+
 describe('Sidebar', () => {
     test('with only first param', () => {
         renderWithTranslation(<Sidebar />);
@@ -12,7 +17,10 @@ describe('Sidebar', () => {
         renderWithTranslation(<Sidebar />);
         const toggleBtn = screen.getByTestId('sidebar-toggle');
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
+        // Проверяем, что sidebar изначально не свернут
+        expect(screen.getByTestId('sidebar')).not.toHaveClass('collapsed');
         fireEvent.click(toggleBtn);
+        screen.debug();
         expect(screen.getByTestId('sidebar')).toHaveClass('collapsed');
     });
 });
