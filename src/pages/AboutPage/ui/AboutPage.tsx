4d2820b28da90486e82c563ee40a5fb797c69f36
Petrushevskiy Maxim <maxinverst@gmail.com>
2025-07-04 21:00:26 +0500

    fix pages Main and About

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,13 +1,7 @@
-import { Counter } from 'entities/Counter';
 import { useTranslation } from 'react-i18next';
 
 export default function AboutPage() {
     const { t } = useTranslation('about');
 
-    return (
-        <div>
-            {t('О сайте')}
-            <Counter />
-        </div>
-    );
+    return <div>{t('О сайте')}</div>;
 }
