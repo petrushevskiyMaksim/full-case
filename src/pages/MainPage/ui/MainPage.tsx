6c61617b4d7badf8361fae36e26ae566856fb396
Petrushevskiy Maxim <maxinverst@gmail.com>
2025-07-07 15:23:04 +0500

    add new Input, add login, form ui

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,13 +1,7 @@
-import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 export default function MainPage() {
     const { t } = useTranslation('main');
-    const [value, setValue] = useState();
-
-    const onChange = () => {
-        setValue(value);
-    };
 
     return <div>{t('Главная страница')}</div>;
 }
