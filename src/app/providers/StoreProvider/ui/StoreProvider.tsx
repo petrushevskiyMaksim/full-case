abc7ec42762c38d3f2bee4c646bdde5a2d34fd07
Petrushevskiy Maxim <maxinverst@gmail.com>
2025-07-14 13:39:09 +0500

    fix storeDecorator

--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -2,16 +2,21 @@ import { Provider } from 'react-redux';
 import { createReduxStore } from '../config/store';
 import { StateSchema } from '../config/StateSchema';
 import { ReactNode } from 'react';
+import { ReducersMapObject } from '@reduxjs/toolkit';
 
 interface StoreProviderProps {
     children?: ReactNode;
     initialState?: Partial<StateSchema>;
+    asyncReducers?: Partial<ReducersMapObject<StateSchema>>;
 }
 
 export const StoreProvider = (props: StoreProviderProps) => {
-    const { children, initialState } = props;
+    const { children, initialState, asyncReducers } = props;
 
-    const store = createReduxStore(initialState as StateSchema);
+    const store = createReduxStore(
+        initialState as StateSchema,
+        asyncReducers as ReducersMapObject<StateSchema>
+    );
 
     return <Provider store={store}>{children}</Provider>;
 };
--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -1,10 +1,22 @@
+import { ReducersMapObject } from '@reduxjs/toolkit';
 import { Decorator } from '@storybook/react-webpack5';
 import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
+import { loginReducer } from 'features/AuthByUsername/model/slice/loginSlice';
+
+const defaultAsyncReducers: Partial<ReducersMapObject<StateSchema>> = {
+    loginForm: loginReducer,
+};
 
 export const StoreDecorator =
-    (state: Partial<StateSchema>): Decorator => (Story) =>
+    (
+        state: Partial<StateSchema>,
+        asyncReducers?: Partial<ReducersMapObject<StateSchema>>
+    ): Decorator => (Story) =>
         (
-            <StoreProvider initialState={state}>
+            <StoreProvider
+                initialState={state}
+                asyncReducers={{ ...defaultAsyncReducers, ...asyncReducers }}
+            >
                 <Story />
             </StoreProvider>
         );
