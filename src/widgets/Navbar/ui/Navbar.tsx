6f83a0f8c8172fa8d853ad0b3ace7057c835965a
Petrushevskiy Maxim <maxinverst@gmail.com>
2025-10-17 19:05:45 +0500

    #commit

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -42,7 +42,6 @@ export const Navbar = memo(({ className }: NavbarProps) => {
                     theme={TextTheme.INVERTED}
                 />
                 <AppLink
-                    className={cls.createBtn}
                     to={RoutePath.articles_create}
                     theme={AppLinkTheme.INVERTED}
                 >
