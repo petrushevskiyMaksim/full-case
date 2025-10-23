1caedf5caf52a26f969d6f6e13986d662c30583f
Petrushevskiy Maxim <maxinverst@gmail.com>
2025-10-23 21:42:38 +0500

    accepted stories

--- src/entities/Country/ui/CountrySelect/CountrySelect.tsx
+++ src/entities/Country/ui/CountrySelect/CountrySelect.tsx
@@ -1,6 +1,5 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
-import { Select } from 'shared/ui/Select/Select';
 import { memo, useCallback } from 'react';
 import { Country } from '../../model/types/country';
 import { ListBox } from 'shared/ui/ListBox/ListBox';
--- src/entities/Currency/ui/CurrencySelect/CurrencySelect.tsx
+++ src/entities/Currency/ui/CurrencySelect/CurrencySelect.tsx
@@ -1,6 +1,5 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
-import { Select } from 'shared/ui/Select/Select';
 import { Currency } from '../../model/types/currency';
 import { memo, useCallback } from 'react';
 import { ListBox } from 'shared/ui/ListBox/ListBox';
