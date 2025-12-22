d3bef2359075c48b660eb57cf8cfa0dbadefba67
Petrushevskiy Maxim <maxinverst@gmail.com>
2025-12-22 14:29:44 +0500

    fix enum theme in storybook config

--- config/storybook/preview.ts
+++ config/storybook/preview.ts
@@ -1,11 +1,11 @@
 import type { Preview } from '@storybook/react-webpack5';
 import StyleDecorator from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
-import { Theme } from '../../src/app/providers/ThemeProvider/lib/ThemeContext';
 import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
 import { StoreDecorator } from '../../src/shared/config/storybook/StoreDecorator/StoreDecorator';
 import { SuspenseDecorator } from '../../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator';
 import { initialize, mswLoader } from 'msw-storybook-addon';
+import { Theme } from '../../src/shared/const/theme';
 
 initialize();
 
